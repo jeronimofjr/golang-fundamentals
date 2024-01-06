6c2c7d7921536fdc2ab227f9379b3371768af948
jeronimofjr <franciscojeronimo@alu.ufc.br>
2024-01-06 01:56:08 -0300

    :sparkles: feat: atualização de exemplos

--- chapter-2/strings.go
+++ chapter-2/strings.go
@@ -3,7 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	// obter o tamanho de uma string
 	fmt.Println(len("Hello, World"))
+	// acessa um caractere da string
 	fmt.Println("Hello, World"[1])
+	// concatenar 2 ou mais strings
 	fmt.Println("Hello, " + "World")
 }
